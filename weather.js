3260b44590dda7968290e01cfe3d1b26b735d715
MMNabeel <124467529+MMNabeel@users.noreply.github.com>
2023-04-24 03:06:54 +0200

    Update

--- weather.js
+++ weather.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
   const query = req.body.cityName
-  const apikey = "b36b6311a61b66b8299fe0394f8f5a36"
+  const apikey = ""
   const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apikey;
   https.get(url, function(response){
     console.log(response.statusCode);
